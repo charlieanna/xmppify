c894f451463e396194753b4f274febbbcc7ad7ba
Charlie <ankothari@gmail.com>
2014-03-29 17:39:56 +0530

    Added support for angular and the gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,6 @@ class User < ActiveRecord::Base
 
   def connect
     @jid, @sid, @rid = RubyBOSH.initialize_session("#{self.encrypted_data}@idlecampus.com", "#{ENV['DOORKEEPER_APP_SECRET']}", "http://idlecampus.com:5280/http-bind")
-    @jid, @sid, @rid
+   return  @jid, @sid, @rid
   end
 end
