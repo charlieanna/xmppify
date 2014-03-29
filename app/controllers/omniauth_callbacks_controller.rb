6bee5431528e12764a96da66fb6c815edc4fcacd
Charlie <ankothari@gmail.com>
2014-03-29 17:50:33 +0530

    Added support for angular and the gem

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,9 +5,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_or_create_for_doorkeeper_oauth(oauth_data)
     @user.update_doorkeeper_credentials(oauth_data)
     @user.save
-    sign_in_as @user
-    ap @user
-    flash[:attacher] = attacher(@user)
+    xmpp_credentials = sign_in_as @user
+    ap xmpp_credentials
+    flash[:attacher] = attacher(xmpp_credentials)
     if @user.identities.find_by(provider:"github").nil?
       @user.identities.create(provider:"github",auth_token:oauth_data.info.github_auth_token,email:oauth_data.info.github_email) 
     end
@@ -18,14 +18,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def sign_in_as(user)
-    p user.connect
+    return user.connect
   end
 
-  def attacher(user)
+  def attacher(xmpp_credentials)
     {
-      jid: user.jid,
-      id: user.sid,
-      rid: user.rid
+      jid: xmpp_credentials.jid,
+      id: xmpp_credentials.sid,
+      rid: xmpp_credentials.rid
     }
   end
 end
