2b02b5d30571e13a9585cf21345fde41dd339631
Charlie <ankothari@gmail.com>
2014-03-29 16:46:58 +0530

    Added support for angular and the gem

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,11 +5,22 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_or_create_for_doorkeeper_oauth(oauth_data)
     @user.update_doorkeeper_credentials(oauth_data)
     @user.save
-    @user.connect
+    sign_in_as @user
+    flash[:attacher] = attacher(self.resource)
     if @user.identities.find_by(provider:"github").nil?
       @user.identities.create(provider:"github",auth_token:oauth_data.info.github_auth_token,email:oauth_data.info.github_email) 
     end
     sign_in @user
     redirect_to dashboard_path
+  end
+
+  private
+
+  def attacher(user)
+    {
+      jid: user.jid,
+      id: user.sid,
+      rid: user.rid
+    }
   end
 end
