36cab22e5ce071f7473edd2243497c5b69fea4a7
Charlieanna <apple@Apples-MacBook-Pro.local>
2014-03-31 00:12:46 +0530

    add encrypted password

--- db/migrate/20140330183230_devise_create_users.rb
+++ db/migrate/20140330183230_devise_create_users.rb
@@ -1,16 +1,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
-    create_table(:users) do |t|
-      ## Database authenticatable
-      t.string :encrypted_password, null: false, default: ""
-
-    
-
-
-      t.timestamps
-    end
-
-    add_index :users, :email,                unique: true
+   
+      add_column :user,:encrypted_password, null: false, default: ""
+      add_index :users, :email,unique: true
     
   end
 end
