029ad0d54f8f35fe071df321914bc7256428e746
Charlieanna <apple@Apples-MacBook-Pro.local>
2014-03-30 14:09:22 +0530

    cleanup the user

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,9 +19,8 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/false'
-
-  # ==> Configuration for any authentication mechanism
+   require 'devise/orm/active_record'
+    # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
   # authenticating a user, both parameters are required. Remember that those
--- lib/xmppify/version.rb
+++ lib/xmppify/version.rb
@@ -1,3 +1,3 @@
 module Xmppify
-  VERSION = "0.0.30"
+  VERSION = "0.0.31"
 end
