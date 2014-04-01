75d8d69d09de7907f11a96f3134da60803cb2f22
ankur <ankothari@gmail.com>
2014-04-02 00:22:18 +0530

    Modified the gemspec

--- lib/xmppify.rb
+++ lib/xmppify.rb
@@ -1,6 +1,11 @@
 require "xmppify/version"
 require 'rails'
 require 'devise'
+require 'omniauth-oauth2'
+require 'paperclip'
+require 'xmpp4r'
+require 'doorkeeper'
+require 'hpricot'
 module Xmppify
   class Engine < Rails::Engine
   end
