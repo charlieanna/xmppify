8ea6055cad1d478630579def5a77b3eb5c4a7d70
ankur <ankothari@gmail.com>
2014-04-18 01:43:52 +0530

    added class name to routeS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Xmppify::Engine.routes.draw do
-  devise_for :users, path_names: {sign_in: "login", signout: "logout"}, controllers: {omniauth_callbacks: "omniauth_callbacks", :invitations => 'invitations', :registrations => "registrations", confirmations: "confirmations"}
+  devise_for :users, path_names: {sign_in: "login", signout: "logout"}, controllers: {omniauth_callbacks: "omniauth_callbacks", :invitations => 'invitations', :registrations => "registrations", confirmations: "confirmations"}, class_name:  'Xmppify::User',
+    module: :devise,
   root to: 'high_voltage/pages#show', id: 'index' 
 end
