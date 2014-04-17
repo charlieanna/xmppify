e976287d73e4c70fb510e7a055ea4bafed940265
ankur <ankothari@gmail.com>
2014-04-17 17:40:25 +0530

    Added omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Xmppify::Engine.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", signout: "logout"}, controllers: {omniauth_callbacks: "omniauth_callbacks", :invitations => 'invitations', :registrations => "registrations", confirmations: "confirmations"}
   root to: 'high_voltage/pages#show', id: 'index' 
 end
